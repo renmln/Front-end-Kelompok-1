03e0b3175ea57dbad39edb37ac99d861b4ebf585
renmln <renamuliani@gmail.com>
2022-07-23 16:18:40 +0700

    update api

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -77,6 +77,8 @@ export const regis = (data) => async (dispatch) => {
       {
         method: "POST",
         headers: {
+          "Access-Control-Allow-Origin":
+            "https://secondhand-backend-k1.herokuapp.com",
           "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
@@ -129,8 +131,6 @@ export const loginWithGoogle = (accessToken) => async (dispatch) => {
       {
         method: "POST",
         headers: {
-          "Access-Control-Allow-Origin":
-            "https://secondhand-backend-k1.herokuapp.com",
           "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
